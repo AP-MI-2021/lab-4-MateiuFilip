94ce006a9964a75af3ecf0077dbb84a330e4ac34
MateiuFilip <filipmateiu@gmail.cpm>
2021-10-18 19:51:27 +0300

    Functii

--- main/main.py
+++ main/main.py
@@ -74,6 +74,11 @@ def test_media_aritmetica_lista_comparata_cu_n():
 
 
 def nr_divizori_proprii(a):
+    '''
+    Returneaza numarul de divizori proprii ai lui a
+    :param a: numar al listei
+    :return: numarul de divizori proprii ai lui a
+    '''
     nr_div = 0
     for i in range(2, a):
         if a % i == 0 and i != a:
@@ -95,22 +100,50 @@ def lista_dupa_adaugarea_nr_de_divizori(l):
 
 
 def test_lista_dupa_adaugarea_nr_de_divizori():
-    assert lista_dupa_adaugarea_nr_de_divizori([19]) == [19, 0]
-    assert lista_dupa_adaugarea_nr_de_divizori([1, 2, 3]) == [1, 0, 2, 0, 3, 0]
+    '''
+    Verifica eficienta functiei lista_dupa_adaugarea_nr_de_divizori()
+    '''
+    assert lista_dupa_adaugarea_nr_de_divizori([19, 22, 31]) == [19, 0, 22, 2, 31, 0]
+    assert lista_dupa_adaugarea_nr_de_divizori([1, 2, 3, 5]) == [1, 0, 2, 0, 3, 0, 5, 0]
     assert lista_dupa_adaugarea_nr_de_divizori([4, 9]) == [4, 1, 9, 1]
+def numar_aparente(x,l):
+    numar=0
+    for i in l:
+        if i == x:
+            numar=numar+1
+    return numar
+def triplet(x,i,l):
+    lista=[]
+    lista.append((x))
+    lista.append((i))
+    lista.append(numar_aparente(x,l))
+    return  lista
 
 
+def liste_tripleti(l):
+    lista_tripleti=[]
+    for i in range(len(l)):
+
+        lista_tripleti.append((triplet(l[i],i,l)))
+    return lista_tripleti
+def test_liste_tripleti():
+    assert liste_tripleti([25, 13, 26, 13])== [[25, 0, 1], [13, 1, 2], [26, 2, 1], [13, 3, 2]]
+    assert liste_tripleti([1,2,3,4,5,1]) == [[1, 0, 2], [2, 1, 1], [3, 2, 1], [4, 3, 1], [5, 4, 1], [1, 5, 2]]
+    assert liste_tripleti([12,13,14,15,12,13,14]) == [[12, 0, 2], [13, 1, 2], [14, 2, 2], [15, 3, 1], [12, 4, 2], [13, 5, 2], [14, 6, 2]]
 def main():
     l = []
     test_numere_prime_din_lista()
     test_media_aritmetica_lista_comparata_cu_n()
     test_lista_dupa_adaugarea_nr_de_divizori()
+    test_liste_tripleti()
     while True:
         print("1.Citire lista.")
         print("2.Afișarea listei după eliminarea numerelor prime din listă.")
         print("3.Afiseaza dacă media aritmetică a numerelor este mai mare decât un număr n dat.")
         print("4.Afișeaza lista prin adăugarea după fiecare element numărul de divizori proprii ai elementului")
-        print("5.")
+        print("5.Afișarea listei obținute din lista inițială în care numerele sunt înlocuite cu un tuplu în care pe")
+        print("prima poziție este numărul, pe a doua poziție va fi indexul elementului din listă, iar pe a treia")
+        print("poziție apare numărul de apariții a numărului.")
         print("-1.Inchide Programul.")
         optiune = input("Dati optiune: ")
 
@@ -126,7 +159,7 @@ def main():
         elif optiune == "4":
             print(lista_dupa_adaugarea_nr_de_divizori(l))
         elif optiune == "5":
-            print()
+            print(liste_tripleti(l))
         elif optiune == "-1":
             print("La revedere!")
             break
